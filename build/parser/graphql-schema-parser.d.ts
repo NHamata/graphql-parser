e4a29c7212e4af3e6a4a5bb538cf324669b5f21e
NHamata <NHamata@>
2021-09-08 01:07:24 -0400

    rebuild

--- build/parser/graphql-schema-parser.d.ts
+++ build/parser/graphql-schema-parser.d.ts
@@ -6,5 +6,4 @@ import { GraphQLSchemaAttrs } from '../typedefs/graphql-schema';
  * @param name the name that corresponds to the GraphQLSchema object that will be generated, this is useful for indexing if generating from multiple schema files
  * @returns  GraphQLSchemaAttrs the attrs was used here so that implicit functional components of the GraphQLschema class was omitted such as the constructor
  */
-declare const generateSchemaObject: (rawGraphqlSchemaText: string, name: string) => GraphQLSchemaAttrs;
-export { generateSchemaObject };
+export declare const generateSchemaObject: (rawGraphqlSchemaText: string, name: string) => GraphQLSchemaAttrs;
